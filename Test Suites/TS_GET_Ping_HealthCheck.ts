a4dd993de07908c094698083c60d93a6123e8aec
Satrio Adi Prawiro <prawiro.96@gmail.com>
2022-12-15 16:49:40 +0700

    make test suites and report for GET_Ping_HealthCheck

---
+++ Test Suites/TS_GET_Ping_HealthCheck.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_GET_Ping_HealthCheck</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3b5bda86-28e6-4da9-84db-179b29d2e6aa</testSuiteGuid>
+   <testCaseLink>
+      <guid>c1cfd484-757a-43ed-9c9f-02b3f1f277c8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GET_Ping_HealthCheck</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
