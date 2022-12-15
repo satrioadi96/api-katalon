14ba34fb05263dbc6a6c4ef706a6fce5523fb5e0
Satrio Adi Prawiro <prawiro.96@gmail.com>
2022-12-15 20:12:48 +0700

    Update POST_Auth with test case, test suite, and Object Repo

---
+++ Test Suites/TS_POST_Auth.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_POST_Auth</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e0ae7c65-35ea-4033-a049-8dbd34c42299</testSuiteGuid>
+   <testCaseLink>
+      <guid>334cdfc5-b47b-422e-920c-27c4293a8d21</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/POST_Auth</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
